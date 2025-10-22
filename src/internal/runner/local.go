bd4c2e811aef8d5128d7408e71f7b160a1edd30b
Touya (nvatuan) <lambda.nvat@gmail.com>
2025-10-23 01:38:36 +0900

    Update templates

--- src/internal/runner/local.go
+++ src/internal/runner/local.go
@@ -2,6 +2,10 @@ package runner
 
 import (
 	"context"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/gh-nvat/gitops-kustomz/src/pkg/diff"
 	"github.com/gh-nvat/gitops-kustomz/src/pkg/kustomize"
@@ -48,9 +52,93 @@ func (r *RunnerLocal) DiffManifests(result *models.BuildManifestResult) (map[str
 }
 
 func (r *RunnerLocal) Process() error {
-	return r.RunnerBase.Process()
+	logger.Info("Process: starting...")
+
+	rs, err := r.BuildManifests()
+	if err != nil {
+		return err
+	}
+	logger.WithField("results", rs).Debug("Built Manifests")
+
+	diffs, err := r.DiffManifests(rs)
+	if err != nil {
+		return err
+	}
+	logger.WithField("results", diffs).Debug("Diffed Manifests")
+
+	policyEval, err := r.Evaluator.GeneratePolicyEvalResultForManifests(r.Context, *rs, []string{})
+	if err != nil {
+		return err
+	}
+	logger.WithField("results", policyEval).Debug("Evaluated Policies")
+
+	reportData := models.ReportData{
+		Service:          r.Options.Service,
+		Timestamp:        time.Now(),
+		BaseCommit:       "base",
+		HeadCommit:       "head",
+		Environments:     r.Options.Environments,
+		ManifestChanges:  diffs,
+		PolicyEvaluation: *policyEval,
+	}
+
+	if err := r.Output(&reportData); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *RunnerLocal) Output(data *models.ReportData) error {
-	return r.RunnerBase.Output(data)
+	logger.Info("Output: starting...")
+	if err := r.outputReportJson(data); err != nil {
+		return err
+	}
+	if err := r.outputReportMarkdown(data); err != nil {
+		return err
+	}
+	logger.Info("Output: done.")
+	return nil
+}
+
+// Exporting report json file to output directory if enabled
+func (r *RunnerLocal) outputReportJson(data *models.ReportData) error {
+	if !r.Options.EnableExportReport {
+		logger.Info("OutputJson: option was disabled")
+		return nil
+	}
+	logger.Info("OutputJson: starting...")
+
+	resultsJson, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	filePath := filepath.Join(r.Options.OutputDir, "report.json")
+	if err := os.WriteFile(filePath, resultsJson, 0644); err != nil {
+		logger.WithField("filePath", filePath).WithField("error", err).Error("Failed to write report data to file")
+		return err
+	}
+	logger.WithField("filePath", filePath).Info("Written report data to file")
+	return nil
+}
+
+// Exporting report markdown file to output directory
+func (r *RunnerLocal) outputReportMarkdown(data *models.ReportData) error {
+	logger.Info("OutputMarkdown: starting...")
+
+	// Render the markdown using templates
+	renderedMarkdown, err := r.Renderer.RenderWithTemplates(r.Options.TemplatesPath, data)
+	if err != nil {
+		logger.WithField("error", err).Error("Failed to render markdown template")
+		return err
+	}
+
+	// Write the rendered markdown to file
+	filePath := filepath.Join(r.Options.OutputDir, "report.md")
+	if err := os.WriteFile(filePath, []byte(renderedMarkdown), 0644); err != nil {
+		logger.WithField("filePath", filePath).WithField("error", err).Error("Failed to write markdown report to file")
+		return err
+	}
+
+	logger.WithField("filePath", filePath).Info("Written markdown report to file")
+	return nil
 }
